783c760ecfe01a09149bdb4cb32f4555eb2145a8
Dev-Sameh <92803501+Sameh-Sany@users.noreply.github.com>
2023-12-13 14:01:45 +0200

    Add earthquake data retrieval and ingestion functionality

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = 3001;
 
+const data = require("./data_management/retrieve_and_ingest_data");
+
+app.use("/ingest_data", data);
+
 app.listen(port, () =>
   console.log(`server is listening at http://localhost:${port}`)
 );
